83ed872837508b81fd73def4e76a62d588cc17d1
nsijwali21 <neerajsijwali7842@gmail.com>
2024-09-24 18:43:39 +0000

    datatypes

--- JS_basics/variable.js
+++ JS_basics/variable.js
@@ -1,7 +1,8 @@
 const accid=12
 let accpass="ns@45"
-console.table([accid,accpass]) //To print more than one variables
-
+city="almora"
+let state;
+console.table([accid,accpass,city,state]) //To print more than one variables
 
 
 //
